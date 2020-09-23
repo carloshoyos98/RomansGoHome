6638195a906a4ed73672acc28a595506a1ab89ba
Carlos Hoyos <choyos@cifpfbmoll.eu>
2020-09-23 20:06:34 +0200

    Enum type creado

---
+++ src/main/java/org/example/RomanSymbols.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public enum RomanSymbols {
+
+    I(1),
+    V(5),
+    X(10),
+    L(50),
+    C(100),
+    D(500),
+    M(1000),
+    IV(4),
+    IX(9),
+    XL(40),
+    XC(90),
+    CD(400),
+    CM(900);
+
+
+    private int value = 0;
+
+    RomanSymbols(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+}
